b865e9d3403661ca7e07b79297c487505d0e663b
Shashika Gurunayake <shashikagurunayake3198@gmail.com>
2024-01-17 09:35:03 +0530

    database updated

--- backend/routes/db.js
+++ backend/routes/db.js
@@ -30,13 +30,84 @@ dbRouter.put('/tempUser', updateVerificationCode);
 
 module.exports = dbRouter;
 
-// const sql = `create table TEMP_USER(
+// const sql = `
+
+// create table STUDENT(
+// 	Reg_number varchar(11) not null,
+//     First_name varchar(20) not null,
+//     Last_name varchar(20) not null,
+//     Department varchar(30) not null,
+//     Email varchar(30) not null,
+//     Batch int not null,
+//     Picture_URL varchar(100),
+//     primary key(Reg_number)
+// );
+
+// create table LECTURER(
+// 	Email varchar(50) not null,
+//     First_name varchar(20) not null,
+//     Last_name varchar(20) not null,
+//     Department varchar(30) not null,
+//     Picture_URL varchar(100),
+//     primary key(Email)
+// );
+
+// create table TEMP_USER(
 // 	Email varchar(50) not null,
 //     Verification_Code varchar(4) not null,
 //     Picture_URL varchar(50),
 //     Verified boolean default false,
 //     primary key(Email)
-// );`;
+// );
+
+// create table TIME_TABLE(
+// 	TT_ID int not null,
+//     Lecturer_Mail varchar(50) not null,
+//     primary key (TT_ID),
+//     foreign key(Lecturer_Mail) references LECTURER(Email)
+// );
+
+// create table MONTH(
+// 	M_ID int not null,
+//     MName varchar(15) not null,
+//     Time_table_ID int not null,
+//     MStatus varchar(20) default "Free",
+//     primary key (M_ID),
+//     foreign key(Time_table_ID) references TIME_TABLE(TT_ID)
+// );
+
+// create table DAY(
+// 	D_ID int not null,
+//     DName varchar(15) not null,
+//     Month_ID int not null,
+//     DStatus varchar(20) default "Free",
+//     Date int not null,
+//     primary key (D_ID),
+//     foreign key(Month_ID) references MONTH(M_ID)
+// );
+
+// create table HOUR(
+// 	H_ID int not null,
+//     Start_time time not null,
+//     End_time time not null,
+//     Day_ID int not null,
+//     HStatus varchar(20) default "Free",
+//     primary key (H_ID),
+//     foreign key(Day_ID) references DAY(D_ID)
+// );
+
+// create table APPOINTMENT(
+// 	Apt_ID int not null,
+//     Lecturer_mail varchar(50) not null,
+//     Hour_ID int not null,
+//     Student_reg varchar(11) not null,
+//     Apt_status varchar(20) default "Not attended",
+//     primary key (Apt_ID),
+//     foreign key(Lecturer_mail) references LECTURER(Email),
+//     foreign key(Hour_ID) references HOUR(H_ID),
+//     foreign key(Student_reg) references STUDENT(Reg_number)
+// );
+// `;
 
 // db.run(sql, (err) => {
 //  if(err){
@@ -46,13 +117,5 @@ module.exports = dbRouter;
 //  }
 // });
 
-// 	Reg_number varchar(11) not null,
-//     First_name varchar(20) not null,
-//     Last_name varchar(20) not null,
-//     Department varchar(30) not null,
-//     Email varchar(30) not null,
-//     Batch int not null,
-//     Picture_URL varchar(100),
-//     primary key(Reg_number)
 
 module.exports = dbRouter;
